2cfe6d510dc038799d94f026e0a7f005a596d2ed
priyatamsai <priyatamsai7@gmail.com>
2020-01-11 22:07:10 +0530

    controllers proto initiated

---
+++ src/main/java/com/example/omnicash/controller/StoreController.java
@@ -0,0 +1,9 @@
+package com.example.omnicash.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/store")
+public class StoreController {
+}
