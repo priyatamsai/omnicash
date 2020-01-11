53e27595882c724e5145522837bf9aa2a725ec15
priyatamsai <priyatamsai7@gmail.com>
2020-01-11 18:09:44 +0530

    initial commit

---
+++ src/test/java/com/example/omnicash/OmnicashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omnicash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnicashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
